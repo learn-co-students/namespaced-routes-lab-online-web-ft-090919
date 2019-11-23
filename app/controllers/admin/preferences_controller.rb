36366f87bcd7b239838b6a336c30cbbd975a3c20
mplichta <bigcatplichta@gmail.com>
2019-11-22 23:45:03 -0700

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,12 @@
+class Admin::PreferencesController < ApplicationController
+    
+    def index
+    end
+
+    # def toggle_artists 
+    #     # allows a link to toggle this setting on/off
+    #     Preference.toggle_artists
+    #     redirect_to :admin_preferences
+    # end
+
+end
